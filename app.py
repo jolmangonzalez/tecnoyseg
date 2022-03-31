1420a94a8e1a7c50feb381e3eb5a8cea47a77820
Jolman González <jolmangonzalez5@gmail.com>
2022-03-31 14:22:27 -0500

    adding styles

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     title = "Inicio"
-    return render_template("home.html")
+    return render_template("home.html", page=title)
 
 
 if __name__ == '__main__':
